1738c1ebda912affc307d60b17ad0483c2c2c0c1
m0nztrum <delafiado@gmail.com>
2024-07-13 19:38:31 +0000

    files controllers

---
+++ controllers/FilesController.js
@@ -0,0 +1,82 @@
+import { ObjectId } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import path from 'path';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class FilesController {
+  static async postUpload(req, res) {
+    const token = req.header('X-TOKEN');
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const { name, type, parentId = 0, isPublic = false, data } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+    const validTypes = ['folder', 'file', 'image'];
+    if (!type || !validTypes.includes(type)) {
+      return res.status(400).json({ error: 'Missing type' });
+    }
+
+    if (type !== 'folder' && !data) {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+    if (parentId !== 0) {
+      if (!ObjectId.isValid(parentId)) {
+        return res.status(400).json({ error: 'Invalid parentId' });
+      }
+      const parentFile = await dbClient.db
+        .collection('files')
+        .findOne({ _id: new ObjectId(parentId) });
+      if (!parentFile) {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+      if (parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+    const newFile = {
+      userId,
+      name,
+      type,
+      isPublic,
+      parentId: parentId === 0 ? 0 : new ObjectId(parentId),
+      localPath: '',
+    };
+
+    if (type !== 'folder') {
+      const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
+
+      const fileName = uuidv4();
+      const localPath = path.join(folderPath, fileName);
+      const fileData = Buffer.from(data, 'base64');
+
+      fs.writeFileSync(localPath, fileData);
+      newFile.localPath = localPath;
+    }
+
+    const result = await dbClient.db.collection('files').insertOne(newFile);
+    return res.status(201).json({
+      id: result.insertedId,
+      userId,
+      name,
+      type,
+      isPublic,
+      parentId,
+      localPath: newFile.localPath,
+    });
+  }
+}
+
+export default FilesController;
