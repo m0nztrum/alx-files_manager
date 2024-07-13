c3c1382f7685de3216dce9b68e6e17e9ff62155a
m0nztrum <delafiado@gmail.com>
2024-07-13 19:43:19 +0000

    docs

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,6 +4,13 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AuthController {
+  /**
+   * Async function to handle user authentication and token generation.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   * @return {Object} The generated token upon successful authentication.
+   */
   static async getConnect(req, res) {
     const authHeader = req.headers.authorization || '';
     if (!authHeader.startsWith('Basic ')) {
@@ -14,7 +21,7 @@ class AuthController {
     const decodedCredentials = Buffer.from(
       base64Credentials,
       'base64',
-    ).toString('utf8');
+    ).toString('utf-8');
     const [email, password] = decodedCredentials.split(':');
 
     if (!email || !password) {
@@ -31,12 +38,21 @@ class AuthController {
     if (!user) {
       return res.status(401).send({ message: 'Unauthorized' });
     }
+
     const token = uuidv4();
     const key = `auth_${token}`;
     await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
     return res.status(200).json({ token });
   }
 
+  /**
+   * Disconnects the user associated with the provided token by deleting
+   * the corresponding Redis key.
+   *
+   * @param {Object} req - The request object containing the token in the 'x-token' header.
+   * @param {Object} res - The response object.
+   * @return {Promise<void>} A Promise that resolves when the user is successfully disconnected.
+   */
   static async getDisconnect(req, res) {
     const token = await req.header('x-token');
     const key = `auth_${token}`;
@@ -45,6 +61,7 @@ class AuthController {
     if (userId) {
       await redisClient.del(key);
     }
+
     res.status(204).send();
   }
 }
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,16 +1,28 @@
 import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
-import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
+  /**
+   * Creates a new user in the database.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   * @return {Promise<void>} A Promise that resolves with a JSON response
+   * containing the user's email and ID if the user is successfully created,
+   * or rejects with a JSON response containing an error message if the user
+   * already exists or if the email or password is missing.
+   */
   static async postNew(req, res) {
-    const { email, password } = req.body;
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing Password' });
     }
 
     const collection = await dbClient.db.collection('users');
@@ -24,11 +36,18 @@ class UsersController {
       email,
       password: hashedPassword,
     });
-
     const { _id } = newUser.ops[0];
     return res.status(201).json({ email, id: _id });
   }
 
+  /**
+   * Retrieves user information based on the provided token.
+   *
+   * @param {Object} req - The request object containing the token.
+   * @param {Object} res - The response object to send back user information.
+   * @return {Promise<void>} A Promise that resolves with the user's email and ID if authorized,
+   * or rejects with an 'Unauthorized' error if no user is found.
+   */
   static async getMe(req, res) {
     const token = req.header('X-Token');
     if (!token) {
@@ -47,7 +66,7 @@ class UsersController {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    return res.status(200).json({ id: user._id, email: user.email });
+    return res.status(200).json({ email: user.email, id: user._id });
   }
 }
 
